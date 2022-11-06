49573c0a6435f35e6f15ff5075ec3734eed52d20
Meysam Azad <MeysamAzad81@gmail.com>
2022-11-07 00:08:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cross Account CloudFront S3
---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eu
+
+ACCOUNT_A_PROFILE=
+ACCOUNT_B_PROFILE=
+
+HOSTED_ZONE_ID=
+DOMAIN_NAME=
+BucketName=
+BucketRegion=
+
+aws --profile ${ACCOUNT_A_PROFILE} cloudformation deploy --stack-name s3-bucket-origin \
+    --template-file 1-account1-s3-bucket.yml \
+    --parameter-overrides BucketName=${BucketName}
+
+aws --profile ${ACCOUNT_B_PROFILE} --region us-east-1 cloudformation deploy --stack-name cloudfront-with-ssl \
+    --template-file 2-account2-cloudfront.yml \
+    --parameter-overrides \
+    HostedZoneId=${HOSTED_ZONE_ID} \
+    DomainName=${DOMAIN_NAME} \
+    BucketName=${BucketName} \
+    BucketRegion=${BucketRegion} \
+    BucketAccountId=$(aws sts --profile ${ACCOUNT_A_PROFILE} get-caller-identity --query Account --output text)
+
+cloudfront_distribution_id=$(aws --profile ${ACCOUNT_B_PROFILE} --region us-east-1 cloudformation describe-stacks --stack-name cloudfront-with-ssl --query "Stacks[0].Outputs[?OutputKey=='CloudFrontDistributionId'].OutputValue" --output text)
+
+aws --profile ${ACCOUNT_A_PROFILE} cloudformation deploy --stack-name s3-bucket-policy \
+    --template-file 3-s3-bucket-policy.yml \
+    --parameter-overrides \
+    BucketName=${BucketName} \
+    DistributionId=${cloudfront_distribution_id} \
+    TargetAccountId=$(aws sts --profile ${ACCOUNT_B_PROFILE} get-caller-identity --query Account --output text)
